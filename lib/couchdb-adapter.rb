c907f8d5665ea3eac2a0f38340bb2b6cd806fe12
Philipp Brüll <b.phifty@gmail.com>
2010-12-08 10:44:11 +0100

    gom-couchdb-adapter-2: Move couchdb layer to another gem

--- lib/couchdb-adapter.rb
+++ lib/couchdb-adapter.rb
@@ -1,5 +1,5 @@
 require 'gom'
-require 'transport'
+require 'couchdb'
 require File.join(File.dirname(__FILE__), "gom", "storage")
 
 GOM::Storage::Adapter.register :couchdb, GOM::Storage::CouchDB::Adapter
