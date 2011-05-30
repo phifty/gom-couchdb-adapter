b8faa53022d4f81465a1dd130f062da1cfa341d3
Philipp Brüll <b.phifty@gmail.com>
2011-05-30 14:10:32 +0200

    fixed pusher spec

--- spec/lib/gom/storage/couchdb/view/pusher_spec.rb
+++ spec/lib/gom/storage/couchdb/view/pusher_spec.rb
@@ -5,9 +5,9 @@ describe GOM::Storage::CouchDB::View::Pusher do
   before :each do
     @database = mock CouchDB::Database
 
-    @class_view = GOM::Storage::Configuration::View::Class.new "Object"
-    @map_reduce_view = GOM::Storage::Configuration::View::MapReduce.new "function(document) { }", "function(key, values) { }"
-    @view_hash = { "test_class_view" => @class_view, "test_map_reduce_view" => @map_reduce_view }
+    @class_view_configuration = GOM::Storage::Configuration::View::Class.new "Object"
+    @map_reduce_view_configuration = GOM::Storage::Configuration::View::MapReduce.new "function(document) { }", "function(key, values) { }"
+    @view_hash = { "test_class_view" => @class_view_configuration, "test_map_reduce_view" => @map_reduce_view_configuration }
 
     @pusher = described_class.new @database, @view_hash
   end
@@ -16,15 +16,18 @@ describe GOM::Storage::CouchDB::View::Pusher do
 
     before :each do
       @design = mock CouchDB::Design, :views => mock(CouchDB::Design::ViewsProxy, :<< => nil), :save => true
-      CouchDB::Design.stub(:new).and_return(@design)
+      CouchDB::Design.stub :new => @design
 
-      @view = mock CouchDB::Design::View
-      CouchDB::Design::View.stub(:new).and_return(@view)
+      @class_view = mock CouchDB::Design::View
+      @map_reduce_view = mock CouchDB::Design::View
+      CouchDB::Design::View.stub :new do |design, name, map_function, reduce_function|
+        { "test_class_view" => @class_view, "test_map_reduce_view" => @map_reduce_view }[name]
+      end
 
       @class_map_reduce_view = mock GOM::Storage::Configuration::View::MapReduce,
         :map => "function(document) { }", :reduce => nil
       @builder = mock GOM::Storage::CouchDB::View::Builder, :map_reduce_view => @class_map_reduce_view
-      GOM::Storage::CouchDB::View::Builder.stub(:new).and_return(@builder)
+      GOM::Storage::CouchDB::View::Builder.stub :new => @builder
     end
 
     it "should initialize the design document" do
@@ -33,24 +36,22 @@ describe GOM::Storage::CouchDB::View::Pusher do
     end
 
     it "should initialize the builder with the class view" do
-      GOM::Storage::CouchDB::View::Builder.should_receive(:new).with(@class_view).and_return(@builder)
+      GOM::Storage::CouchDB::View::Builder.should_receive(:new).with(@class_view_configuration).and_return(@builder)
       @pusher.perform
     end
 
-    it "should use the builder to create a map reduce view out of the class view" do
+    it "should use the builder to generate a map reduce view out of the class view" do
       @builder.should_receive(:map_reduce_view).and_return(@class_map_reduce_view)
       @pusher.perform
     end
 
-    it "should initialize the created map reduce view" do
-      CouchDB::Design::View.should_receive(:new).with(@design, "test_class_view", "function(document) { }", nil).once.and_return(@view)
-      CouchDB::Design::View.should_receive(:new).once.and_return(@view)
+    it "should add the generated map reduce view" do
+      @design.views.should_receive(:<<).with(@class_view)
       @pusher.perform
     end
 
-    it "should initialize the map reduce view" do
-      CouchDB::Design::View.should_receive(:new).once.and_return(@view)
-      CouchDB::Design::View.should_receive(:new).with(@design, "test_map_reduce_view", "function(document) { }", "function(key, values) { }").and_return(@view)
+    it "should add the map reduce view" do
+      @design.views.should_receive(:<<).with(@map_reduce_view)
       @pusher.perform
     end
 
